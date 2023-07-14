39b3a484484e407cc0c1c5f777656f83e2dee11c
arthur <a.lannelucq@gmail.com>
2023-07-14 14:59:09 +0200

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'event-form-app';
+
+  form = new FormGroup({
+    firstname: new FormControl(''),
+    lastname: new FormControl(''),
+    email: new FormControl(''),
+    birthDate: new FormControl<Nullable<string>>(null),
+    adultTicketCount: new FormControl<Nullable<number>>(1),
+    childTicketCount: new FormControl<Nullable<number>>(2),
+  });
+
+  onSubmit() {
+    console.log('Form has been submitted !');
+  }
 }
